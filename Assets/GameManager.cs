63860bc14ce6d13800d31df52006a112ec574034
Nilej24 <nmanalo543@gmail.com>
2023-08-08 03:10:21 +0100

    added car selection system using scriptable objects and playerprefs

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CarPrefabs carPrefabs; 
+    public CinemachineVirtualCamera vcam;
+
+    private GameObject carPrefab;
+    public GameObject playerCar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("SelectedCar", 0);
+
+        // get player's selected car
+        int carIndex = PlayerPrefs.GetInt("SelectedCar");
+        carPrefab = carPrefabs.cars[carIndex];
+
+        // spawn the car
+        playerCar = Instantiate(carPrefab, Vector3.zero, Quaternion.identity);
+
+        // give the car to the camera
+        vcam.m_Follow = playerCar.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
